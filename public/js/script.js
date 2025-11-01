f1f43a633ba2ed0d20352ffcca58183b6a1555a1
radamian <raluca.damian@endava.com>
2025-11-01 16:34:21 +0200

    Add files via upload

---
+++ public/js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("helloBtn");
+  btn.addEventListener("click", () => {
+    alert("Hello from the Kindergarten Website!");
+  });
+});
